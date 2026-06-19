447bf45c4f0f1ef1f95f90e29ac37a8fc152f4dd
iqrxfati20-max <iqrxfati20@gmail.com>
2026-06-20 00:23:19 +0530

    Update supabase.ts

--- artifacts/gorakhpur-diaper-house/supabase.ts
+++ artifacts/gorakhpur-diaper-house/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://cwjzbpbekkxqllkcnxtf.supabase.co
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3anpicGJla2t4cWxsa2NueHRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MDc2MzIsImV4cCI6MjA5NTE4MzYzMn0.aa5HsHtRGCp1ytTW0b2Vk0QGJIfe30JBYjO9gjFScno'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
